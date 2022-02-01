364f713dce8d6efb4024e63219712707724ed003
roelma2000 <roelma2000@gmail.com>
2022-02-01 00:36:07 -0500

    modified

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('lab1-expressportfolio:server');
 var http = require('http');
 
