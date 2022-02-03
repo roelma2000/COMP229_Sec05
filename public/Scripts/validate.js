a776aa0909cb583a20bdde6f014d46899a6efdc4
roelma2000 <roelma2000@gmail.com>
2022-02-03 16:56:28 -0500

    Fix Toggler
    Change Toggler of Bootstrap 5

---
+++ public/Scripts/validate.js
@@ -0,0 +1,8 @@
+function submitMe() {
+    alert("Thank you. I will respond to you ASAP. Here are the details you entered:" + "\n\n"
+        +"Your Email: " + document.getElementById("email").value + "\n"
+        + "Your Last Name: " + document.getElementById("lname").value + "\n"
+        + "Your First Name: " + document.getElementById("fname").value + "\n"
+        + "Your Message:\n           " + document.getElementById("message").value + "\n"
+    );
+}
